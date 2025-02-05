8aaf297b94ad88cad6366e6b780a1c4fdfb4e306
GuillaumeRiverin <155865827+GuillaumeRiverin@users.noreply.github.com>
2025-02-04 20:00:33 -0500

    Création des dossier API et Stratégies et des fichier Main.py et , dans le dossier API, Stock.py

---
+++ API's/stock.py
@@ -0,0 +1,10 @@
+#API des actions
+# 1. Lire le prix d'une action, maintenant et dans le passé
+# 2. Faire toutes les actions en lien avec l'achat d'une action
+# 3. Faire toutes les actions en lien avec la vente d'une action
+
+
+
+
+
+# IMPORTS
